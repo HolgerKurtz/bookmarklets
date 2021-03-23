031752b8821473585066a708deb714da3bdc5f48
Holger Kurtz <kulturdata@gmail.com>
2021-03-23 19:27:22 +0100

    added sistrix api. not working. cors error

--- bookmarklet.js
+++ bookmarklet.js
@@ -21,4 +21,21 @@
 
     }
     console.log(meta_info);
+
+    // sistrix part
+
+    var api_key = prompt("Your SISTRIX API KEY");
+    var url = "&url=" + window.location.href;
+    var query = "https://api.sistrix.com/keyword.domain.seo?api_key=";
+    var search = "&date=now&num=5"
+    var api_url = query + api_key + url + search;
+    console.log(api_url);
+
+    // I am getting a CORS error:
+    fetch(api_url)
+        .then(response => response.json())
+        .then(data => console.log(data));
+
 })();
+
+
