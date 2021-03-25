ddf99863567397f7aee9beb8dd150fe7fe8543f9
Holger Kurtz <kulturdata@gmail.com>
2021-03-25 19:46:32 +0100

    extension now working but still cors problem

--- background.js
+++ background.js
@@ -1,6 +1,15 @@
-let color = '#3aa757';
+let api_url = "";
 
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.set({ color });
-    console.log('Default background color set to %cgreen', `color: ${color}`);
-});
+
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        console.log(request);
+        console.log(request.url)
+        fetch(request.url)
+            .then(result => result.json())
+            .then(data => console.log(data))
+        return true;
+
+    })
+
+
--- popup.js
+++ popup.js
@@ -1,15 +1,30 @@
 
 let seoButton = document.getElementById("seoButton");
 
-
 seoButton.addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
-        function: addSeoTags,
+        function: addSeoTags
+    });
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: sistrix
     });
 });
+
+function sistrix() {    // sistrix part
+    var api_key = prompt("Your SISTRIX API KEY");
+    var url = "&url=" + window.location.href;
+    var query = "https://api.sistrix.com/keyword.domain.seo?api_key=";
+    var search = "&date=now&num=5&format=json"
+    var api_url = query + api_key + url + search;
+    console.log(api_url);
+    chrome.runtime.sendMessage({ url: api_url }, function (response) {
+        console.log(response);
+    });
+}
 
 function addSeoTags() {
     console.log("SEO BUTTON CLICKED");
