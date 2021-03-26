69aeb4b5d53f0c793bf61f584718429bca3e6da9
Holger Kurtz <kulturdata@gmail.com>
2021-03-26 21:04:23 +0100

    working extension

--- background.js
+++ background.js
@@ -1,15 +1,6 @@
-let api_url = "";
-
-
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
-        console.log(request);
-        console.log(request.url)
-        fetch(request.url)
-            .then(result => result.json())
-            .then(data => console.log(data))
-        return true;
-
+        console.log(request)
     })
 
 
--- popup.js
+++ popup.js
@@ -1,6 +1,17 @@
 
 let seoButton = document.getElementById("seoButton");
 
+chrome.storage.local.get(['key'], function (result) {
+    createTable(result.key);
+});
+
+chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+    let url = tabs[0].url;
+    url_txt = document.getElementById("url");
+    url_txt.innerText = url;
+    // use `url` here inside the callback because it's asynchronous!
+});
+
 seoButton.addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
@@ -15,15 +26,50 @@ seoButton.addEventListener("click", async () => {
 });
 
 function sistrix() {    // sistrix part
+
+    // get url and api key
     var api_key = prompt("Your SISTRIX API KEY");
     var url = "&url=" + window.location.href;
     var query = "https://api.sistrix.com/keyword.domain.seo?api_key=";
     var search = "&date=now&num=5&format=json"
     var api_url = query + api_key + url + search;
     console.log(api_url);
-    chrome.runtime.sendMessage({ url: api_url }, function (response) {
-        console.log(response);
-    });
+
+    // handle cors needs klick on url
+    let corsUrl = "https://cors-anywhere.herokuapp.com/" + api_url;
+    console.log(corsUrl);
+
+    // fetching data and preparing json
+    async function fetchSistrixJSON() {
+        const response = await fetch(corsUrl);
+        const json = await response.json();
+        console.log(json);
+        const sisData = json.answer[0].result;
+        return sisData;
+    }
+
+    fetchSistrixJSON()
+        .then(sisData => {
+            console.log(sisData);
+            kwList = [];
+            positionList = [];
+            trafficList = [];
+            sistrixObject = {};
+            for (keyword of sisData) {
+                kwList.push(keyword.kw);
+                positionList.push(keyword.position);
+                trafficList.push(keyword.traffic);
+            }
+            sistrixObject["keyword"] = kwList;
+            sistrixObject["position"] = positionList;
+            sistrixObject["traffic"] = trafficList;
+            return sistrixObject;
+        })
+        .then(sistrixObject => {
+            // show data in popup
+            chrome.storage.local.set({ key: sistrixObject }, function () {
+            });
+        });
 }
 
 function addSeoTags() {
@@ -51,3 +97,35 @@ function addSeoTags() {
     }
     console.log(meta_info);
 };
+function createTable(resultKey) {
+    console.log(resultKey);
+    let table = document.querySelector("table");
+    console.log(table);
+    // https://www.valentinog.com/blog/html-table/
+
+
+    function createTableHead() {
+        let thead = table.createTHead();
+        let row = thead.insertRow();
+        let keys = Object.keys(resultKey);
+
+        for (let key of keys) {
+            let th = document.createElement('th');
+            let text = document.createTextNode(key);
+            th.appendChild(text);
+            row.appendChild(th);
+            let seoData = resultKey[key];
+
+            for (let data of seoData) {
+                let cell = row.insertCell();
+                let text = document.createTextNode(data);
+                cell.appendChild(text);
+
+            }
+            row = table.insertRow();
+        }
+    }
+    createTableHead();
+
+
+}
